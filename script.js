4f32e8aab404d197dc9231d2cea4ef3783c1b181
simonelupone <simone.lupone@gmail.com>
2025-03-17 16:57:03 +0100

    [feat]: add condition for discount

--- script.js
+++ script.js
@@ -26,3 +26,18 @@ console.log('Prezzo base:', price);
 // e la salvo in una variabile
 let userAge = prompt('Inserire età');
 console.log('Età utente:', userAge);
+
+// imposto una condizione per stabilire il prezzo finale
+// in base all'età dell'utente
+let finalPrice;
+if (userAge < under) {
+    finalPrice = price * underCoeff;
+    console.log('Prezzo per under:', finalPrice);
+} else if (userAge > over) {
+    finalPrice = price * overCoeff;
+    console.log('Prezzo per over:', finalPrice);
+} else {
+    finalPrice = price;
+    console.log('Prezzo base:', finalPrice);
+}
+console.log('finalPrice', parseFloat(finalPrice.toFixed(2)));
