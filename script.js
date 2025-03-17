08e70221430d1e27691ea439251064c3267e128f
simonelupone <simone.lupone@gmail.com>
2025-03-17 16:51:59 +0100

    [feat]: add under 18 and over 65 cutoff

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 // da applicare ai km per determinare il prezzo base
 let priceCoeff = 0.21;
 console.log('Coefficiente prezzo base:', priceCoeff);
+
+// inizializzo le variabili per calcolare lo sconto
+let underCoeff = 0.80;
+let overCoeff = 0.60;
+console.log('Coefficiente prezzo under:', underCoeff, 'Coefficiente prezzo over:', overCoeff);
