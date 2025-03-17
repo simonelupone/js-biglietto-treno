b252e6bb50c4dc790c710a4b67b0f0c1c16927fd
simonelupone <simone.lupone@gmail.com>
2025-03-17 16:51:09 +0100

    [feat]: add price coeff

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log('Hello World!')
+// inizializzo una variabile con il coefficiente
+// da applicare ai km per determinare il prezzo base
+let priceCoeff = 0.21;
+console.log('Coefficiente prezzo base:', priceCoeff);
